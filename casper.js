a466aa926045ba84b3b6bca0708431a7b5a9c542
Tudor <iovitatudor21@gmaill.com>
2023-06-28 14:37:26 +0300

    Initial commit

---
+++ casper.js
@@ -0,0 +1,5 @@
+let iframe = document.createElement("iframe");
+iframe.setAttribute("src", "https://front.artcoin.media/");
+iframe.style.width = "640px";
+iframe.style.height = "480px";
+document.body.appendChild(iframe);
